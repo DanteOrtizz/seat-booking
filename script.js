8e670e9e42df6a1d0feab3d5b3488cb755b304ea
Dante Ortiz <verlacdante@gmail.com>
2023-10-31 18:06:13 -0700

    Populate UI with stored items

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 function setMovieDate(movieIndex, moviePrice) {
@@ -24,6 +26,24 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if(selectedSeats.indexOf(index) > -1) {
+          seat.classList.add('selected');
+        }
+      });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
     setMovieDate(e.target.selectedIndex, e.target.value);
@@ -41,3 +61,4 @@ container.addEventListener('click', e => {
     }
 });
 
+updateSelectedCount();
